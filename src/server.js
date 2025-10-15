b44f760c17ceeb99e200f89025ec18c6f04ebc50
caillet232 <enzo.caillet44@gmail.com>
2025-10-15 10:00:04 +0200

    api

--- src/server.js
+++ src/server.js
@@ -1,11 +1,43 @@
-import http from  'http'
+/*import http from  'http'
 import { getEnvironmentData } from 'worker_threads'
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const server = http.createServer((req,res) => {
-    res.end('yo les ptit loup !')
+    res.end('yo les ptit fou !')
 })
 
-server.listen(PORT || 3000, () => {
-    console.log(`serveur running on http://localhost: ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`serveur running on http://localhost:${PORT}`)
 }) 
+*/
+
+import express from 'express'
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.json())
+
+app.get('/questions', (req,res) => {
+    res.status(200).send([
+        {
+            id:"1",
+            question:"Allez body count",
+            answer:"15",
+        },
+    ])
+})
+
+app.post('/questions', (req,res) => {
+    const { question, answer } = req.body
+    
+    if(!question || !answer){
+        return res(400).send({error: 'Question and answer requiered'})
+    }
+
+    res.status(201).send({message: 'bien joué ma reine'})
+})
+
+app.listen(PORT, () =>  {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
